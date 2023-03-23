8a776df650b8c5dab1125712c35777f32d18dc16
Hoang Trung <hoangtrungcongviec@gmail.com>
2023-03-23 15:47:20 +0700

    update detect video

--- DetectFromVideo.py
+++ DetectFromVideo.py
@@ -9,28 +9,6 @@ import geocoder
 import json
 import argparse
 from ChatBot import ChatBot
-# load model
-start_time = time.time()
-sys.path.append('yolov7')
-from models.experimental import attempt_load
-model = attempt_load(r"weight\best_216.pt", map_location=torch.device('cuda:0'))  # load FP32 model
-print("Done after %s seconds" % (time.time() - start_time))
-
-
-# load config
-classes_to_filter = None
-opt  = {
-    "model" : model,
-    "weights": r"yolov7\runs\train\last.pt", # Path to weights file default weights are for nano model
-    "yaml"   : r"yolov7\data\mydataset.yaml",
-    "img-size": 640, # default image size
-    "conf-thres": 0.1, # confidence threshold for inference.
-    "iou-thres" : 0.45, # NMS IoU threshold for inference.
-    "device" : '1',  # device to run our model i.e. 0 or 0,1,2,3 or cpu
-    "classes" : classes_to_filter  # list of classes to filter or None
-
-}
-
 def export_information(number_potholes,frame_number,time,path_export):
     '''
     Export information to json file
@@ -46,16 +24,39 @@ def export_information(number_potholes,frame_number,time,path_export):
         json.dump(info, outfile,indent=4)
     return info
 
-
-
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser()
+    parser.add_argument('--weight', type=str, default='weight/best.pt', help='path to weight file')
     parser.add_argument('--video', type=str, default='0', help='video url. If you do not provide any video url, the program will use your webcam')
     parser.add_argument('--trackingfolder', type=str, default='tracking_data', help='folder to save tracking data')
     parser.add_argument('--bottoken', type=str, default='6069180604:AAFWUcioYoE_8-myXYsKB5IwxcEKGVtAUPI', help='impute bot token')
     parser.add_argument('--chatid', type=str, default='-1001833973135', help='impute bot id')
-    
     config = parser.parse_args()
+    
+    
+    # load model
+    start_time = time.time()
+    sys.path.append('yolov7')
+    from models.experimental import attempt_load
+    path_weight = config.weight
+    model = attempt_load(path_weight, map_location=torch.device('cuda:0'))  # load FP32 model
+    print("Loading done after %s seconds" % (time.time() - start_time))
+
+
+    # load config
+    classes_to_filter = None
+    opt  = {
+        "model" : model,
+        "weights": r"yolov7\runs\train\last.pt", # Path to weights file default weights are for nano model
+        "yaml"   : r"yolov7\data\mydataset.yaml",
+        "img-size": 640, # default image size
+        "conf-thres": 0.1, # confidence threshold for inference.
+        "iou-thres" : 0.45, # NMS IoU threshold for inference.
+        "device" : '1',  # device to run our model i.e. 0 or 0,1,2,3 or cpu
+        "classes" : classes_to_filter  # list of classes to filter or None
+
+    }    
     # Setup chat bot
     bot_token = config.bottoken#'6069180604:AAFWUcioYoE_8-myXYsKB5IwxcEKGVtAUPI'
     chat_id =  config.chatid##-1001833973135
@@ -96,17 +97,21 @@ if __name__ == "__main__":
     currentFrame = 0
     prevTime = time.time()
     currentTime = 0
-    
+    start_time = 0
+    pre_time = 0
     
     # # loop for extracting all frame in video and outloop when click 'q'
     while vid.isOpened():
         success, frame = vid.read()
         detector = Detector(frame, opt)
+        
         img,number_potholes = detector.detect_plotbox()
-        cv2.imshow(f'Checking camera',frame)
+        # cv2.imshow(f'Checking camera',frame)
         currentFrame+=1
         currentTime = time.time()
-        # check if frame is not over 10
+       
+        
+        #check if frame is not over 1
         if number_potholes > 1:
             
             # create folder to save frame information
